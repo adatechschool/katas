3ed771a382789934b6b1d5e77d9f598b1d229cea
KheOps <kheops@ceops.eu>
2019-11-26 11:37:53 +0100

    Création de la liste d'entiers pas encore triée
    Signed-off-by: KheOps <kheops@ceops.eu>

--- 2019-11-26 Lecture et écriture de fichier, tri d'entiers/tri.py
+++ 2019-11-26 Lecture et écriture de fichier, tri d'entiers/tri.py
@@ -4,8 +4,12 @@ import csv
 
 with open ("désordre", "r") as désordre:
   contenu = désordre.read()
+  liste_nombres = []
   for nombre_chaine in contenu.split():
     nombre_chaine = int(nombre_chaine)
+    liste_nombres += [nombre_chaine]
+  
+  print (liste_nombres)
   #print (contenu.split())
   #print (contenu)
   if désordre.closed:
