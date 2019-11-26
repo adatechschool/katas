50707bdf027cc360fa1562414c0aa1baf41a8929
KheOps <kheops@ceops.eu>
2019-11-26 11:11:59 +0100

    Ajout conversion en liste en chaque élément en entier
    Signed-off-by: KheOps <kheops@ceops.eu>

--- 2019-11-26 Lecture et écriture de fichier, tri d'entiers/tri.py
+++ 2019-11-26 Lecture et écriture de fichier, tri d'entiers/tri.py
@@ -3,7 +3,11 @@
 import csv
 
 with open ("désordre", "r") as désordre:
-  print (désordre.read())
+  contenu = désordre.read()
+  for nombre_chaine in contenu.split():
+    nombre_chaine = int(nombre_chaine)
+  #print (contenu.split())
+  #print (contenu)
   if désordre.closed:
     print ("je sais pas quoi")
   
