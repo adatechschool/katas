ecf0902b2e43335946f017f4b1ec6a708c10c85e
KheOps <kheops@ceops.eu>
2019-11-26 11:55:51 +0100

    Clarifications du code
    Signed-off-by: KheOps <kheops@ceops.eu>

--- 2019-11-26 Lecture et écriture de fichier, tri d'entiers/tri.py
+++ 2019-11-26 Lecture et écriture de fichier, tri d'entiers/tri.py
@@ -3,15 +3,19 @@
 import csv
 
 with open ("désordre", "r") as désordre:
+  if désordre.closed:
+    print ("je sais pas quoi")
+    return
+
   contenu = désordre.read()
   liste_nombres = []
   for nombre_chaine in contenu.split():
-    nombre_chaine = int(nombre_chaine)
-    liste_nombres += [nombre_chaine]
-  
-  print (liste_nombres)
-  #print (contenu.split())
-  #print (contenu)
-  if désordre.closed:
-    print ("je sais pas quoi")
-  
+    nombre_converti = int(nombre_chaine)
+    liste_nombres += [nombre_converti]
+
+  liste_nombres.sort()
+
+with open ("croissant", "w") as croissant:
+  for nombre in liste_nombres:
+    nombre = str(nombre)
+    croissant.write(nombre + " ")
