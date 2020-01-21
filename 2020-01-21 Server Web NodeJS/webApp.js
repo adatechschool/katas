5b918ed29ec11ab75637feb38099446523d9508c
lydiasan <lyd.sanaa@gmail.com>
2020-01-21 15:54:26 +0100

    Une redirection de la page acceuil vers parametres

--- 2020-01-21 Server Web NodeJS/webApp.js
+++ 2020-01-21 Server Web NodeJS/webApp.js
@@ -2,11 +2,16 @@ const http = require('http');
 const url = require('url');
 
 var cheminAccueil = "accueil.html"
+var nomHtml = url.pathname
+
+console.log(nomHtml);
 
 http.createServer(function (requete, reponse) {
-  reponse.write(requete.url);
-  reponse.write(url.parse(requete.url).pathname);
-  if ( url == "acceuil.html") {
+var pathname =url.parse(requete.url).pathname;
+console.log(pathname);
+	// reponse.write(requete.url);
+ // reponse.write(url.parse(requete.url).pathname);
+  if ( pathname == "/") {
     reponse.writeHead(200);
     reponse.end('Page found but has no point.');
   } else {
@@ -14,5 +19,6 @@ http.createServer(function (requete, reponse) {
     reponse.end('Page not found');
   }
 
+console.log(url.parse(requete.url).pathname);
 
 }).listen(8080);
