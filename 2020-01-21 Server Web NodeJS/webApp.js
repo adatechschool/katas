3242e23fb09d7597b3b93fa3cc76eb0ed91a5ace
Leirda01 <adrieldj@orange.fr>
2020-01-21 12:58:56 +0100

    Ajoute require('url') et commit ce qui marche pas.

--- 2020-01-21 Server Web NodeJS/webApp.js
+++ 2020-01-21 Server Web NodeJS/webApp.js
@@ -1,8 +1,18 @@
-var http = require('http');
+const http = require('http');
+const url = require('url');
 
 var cheminAccueil = "accueil.html"
 
-http.createServer(function (req, res) {
-  res.write(cheminAccueil); 
-  res.end();
+http.createServer(function (requete, reponse) {
+  reponse.write(requete.url);
+  reponse.write(url.parse(requete.url).pathname);
+  if ( url == "acceuil.html") {
+    reponse.writeHead(200);
+    reponse.end('Page found but has no point.');
+  } else {
+    reponse.writeHead(404);
+    reponse.end('Page not found');
+  }
+
+
 }).listen(8080);
