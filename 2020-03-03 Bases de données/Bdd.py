0134878c30a709f92e7cadad710441784f5cd85b
KheOps <kheops@ceops.eu>
2020-03-03 12:12:06 +0100

    Code base de données
    Signed-off-by: KheOps <kheops@ceops.eu>

---
+++ 2020-03-03 Bases de données/Bdd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Objectifs :
+# - se connecter à une bdd sqlite3
+# - tester si une table « Produits » existe, et si non la créer
+# - insérer un produit
+# - lister les produits
+
+import sqlite3
+
+conn = sqlite3.connect('notre-base-de-données.sql3')
+
+cur = conn.cursor()
+
+cur.execute ('''SELECT * FROM sqlite_master WHERE type = 'table' AND name = 'Produits' ''')
+
+# Retourne None si la table n'existe pas
+# Retourne des données sinon
+r = cur.fetchone()
+
+if r == None:
+  cur.execute ('CREATE TABLE Produits (prix float, code int, description text)')
+  print ("La table n'existait pas alors je l'ai créée")
+
