f277b7ee5471ad5be8e80c7c17dae8de4ec1e82f
Leirda01 <adrieldj@orange.fr>
2020-04-22 15:38:34 +0200

    Use `union' to get each square unique in world

--- 2020-04-22 La fourmi de Langton en Haskell/ant.hs
+++ 2020-04-22 La fourmi de Langton en Haskell/ant.hs
@@ -1,8 +1,10 @@
+import Data.List
+
 data Color = White | Black deriving (Show, Eq)
 
 data Square = Square { x :: Int
                      , y :: Int
                      , color :: Color
-                     }
+                     } deriving (Show, Eq)
 
 type World = [Square]
